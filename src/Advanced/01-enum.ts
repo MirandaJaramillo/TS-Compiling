79f85802e9108dc9f9fbc1443a8e89bf7c58fc54
Miranda Jaramillo Morales <mirandajaramillo@MacBook-Pro-de-Miranda-2.local>
2022-10-11 12:17:13 -0500

    Added enums concept into Advanced files

---
+++ src/Advanced/01-enum.ts
@@ -0,0 +1,25 @@
+// enums are like list where we can add keys and values, so  we can
+//have a predefined set
+
+// enum --> specify descriptive constants and associate
+
+enum ROLES {
+  //ADMIN ->
+  ADMIN = "admin",
+  SELLER = "seller",
+  CUSTOMER = "customer",
+
+}
+  // Alias Type here
+  type User = {
+    username: string;
+    role: ROLES;
+  }
+
+  const nicoUser: User = {
+    username: 'nicobytes',
+    role: ROLES.SELLER
+  }
+
+  // we will be using capacitor
+  // https://capacitorjs.com/
