217e0e979b2b2f22caca4f3d366ca8182d786f74
Miranda Jaramillo Morales <mirandajaramillo@MacBook-Pro-de-Miranda-2.local>
2022-10-13 17:23:48 -0500

    Getter accessors and using them as properties files for Leap year function

--- src/POO/06-getters.ts
+++ src/POO/06-getters.ts
@@ -1,4 +1,3 @@
-import { isAfter } from "date-fns";
 
 export class MyDate {
 
@@ -6,16 +5,16 @@ export class MyDate {
 
     public year: number = 1993,
     public month: number = 7,
-    public day:number = 9){}
+    private _day:number = 9){}
 
   printFormat(): string {
-    const day = this.addPadding(this.day);
+    const day = this.addPadding(this._day);
     const month = this.addPadding(this.month);
     return `${day}/${month}/${this.year}`;
   }
 
   add(amount:number, type:"days" | "months" | "years"){
-    if(type === "days") this.day += amount;
+    if(type === "days") this._day += amount;
     if(type === "months") this.month += amount;
     if(type === "years") this.year += amount;
   }
@@ -24,13 +23,35 @@ export class MyDate {
     if(value < 10) return `0${value}`;
     return `${value}`;
   }
-  getDay() {
-    return this.day;
-  }
 
+  // if we define a prop as private and then a method with the same name
+  // it will throw an error
+  // get is a function that access the prop
+  get day() {
+    return this._day;
+  }
+// GET ACCESSOR WORKS AS PROPERTY // GET ACCESSOR WORKS AS PROPERTY // GET ACCESSOR WORKS AS PROPERTY
+// A GETTER MUST RETURN SOMETHING!! UWUW
+  get isLeapYear():boolean {
+    if (this.year % 400 === 0) return true;
+    if (this.year % 100 === 0) return false;
+    return this.year % 4 === 0;
+  }
 };
 
 
 const myDate = new MyDate(1993,7,9);
 console.log(myDate.printFormat());
-console.log(myDate.getDay());
+//console.log(myDate.getDay());
+
+console.log(myDate.day);
+console.log(myDate.isLeapYear)
+
+const myDate2 = new MyDate(2000,7,9);
+console.log(myDate2.isLeapYear)
+
+const myDate3 = new MyDate(2001,7,9);
+console.log(myDate3.isLeapYear)
+
+const myDate4 = new MyDate(2004,7,9);
+console.log(myDate4.isLeapYear)
